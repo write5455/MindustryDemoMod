81c388c2300dfd3d1b883d949d5fcf1a23c5e868
write5455 <771317600@qq.com>
2025-08-20 23:46:16 +0800

    none

--- src/MyContent/ModPower.java
+++ src/MyContent/ModPower.java
@@ -1,23 +1,26 @@
 package MyContent;
 
+import mindustry.content.Fx;
 import mindustry.content.Items;
+import mindustry.gen.Sounds;
 import mindustry.mod.Mod;
 import mindustry.type.Category;
 import mindustry.type.ItemStack;
 import mindustry.world.Block;
 import mindustry.world.blocks.power.*;
-import mindustry.world.meta.Stat;
-import mindustry.world.meta.StatUnit;
+import mindustry.world.consumers.ConsumeItemExplode;
+import mindustry.world.consumers.ConsumeItemFlammable;
+import mindustry.world.draw.*;
 
 import static MyContent.ModItems.apple;
 
 public class ModPower extends Mod{
     public static Block appleGenerator;
+    public static Block bb;
 
     public static void load() {
         // 创建新发电机
-        appleGenerator = new PowerGenerator("appleGenerator"){
-            {
+        appleGenerator = new ConsumeGenerator("appleGenerator"){{
                 // 设置发电机属性
                 requirements(
                         Category.power,
@@ -28,21 +31,31 @@ public class ModPower extends Mod{
                 );
 
                 powerProduction = 5f; // 发电量
+                itemDuration = 60*14f; //发电时间
                 itemCapacity = 200; // 物品容量
-                size = 4; // 方块大小
-                //hasItems = true;
+                size = 1; // 方块大小
+                hasItems = true;
 
                 // 设置消耗物品和发电时间
-                consumeItem(Items.coal, 14/210); // 每秒消耗1个物品
-            }
-
-            @Override
-            public void setStats(){
-                super.setStats();
-                // 自定义统计信息
-                stats.add(Stat.productionTime, 210f/14f, StatUnit.seconds);
+                consumeItem(apple, 1); // 每秒消耗1个物品
             }
         };
+
+        bb = new ConsumeGenerator("bb-generator"){{
+            requirements(Category.power, ItemStack.with(Items.copper, 25, Items.lead, 15));
+            powerProduction = 10f;
+            itemDuration = 120f;
+
+            ambientSound = Sounds.smelter;
+            ambientSoundVolume = 0.03f;
+            generateEffect = Fx.generatespark;
+
+            consume(new ConsumeItemFlammable());
+            consume(new ConsumeItemExplode());
+
+            drawer = new DrawMulti(new DrawDefault(), new DrawWarmupRegion());
+        }};
+
         appleGenerator.load();
     }
 }
