b9e14c549e13fea2330c24c0ed5c8908b70816cc
write5455 <771317600@qq.com>
2025-08-20 18:00:02 +0800

    none

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -21,7 +21,7 @@ public class ExampleJavaMod extends Mod{
                 BaseDialog dialog = new BaseDialog("frog");
                 dialog.cont.add("behold").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("Demo-frog")).pad(20f).row();
+                dialog.cont.image(Core.atlas.find("test-frog")).pad(20f).row();
                 dialog.cont.button("了解", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
