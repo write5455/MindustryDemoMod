db6752330392711a6b4954dadb29076515feb238
write5455 <771317600@qq.com>
2025-08-20 20:10:09 +0800

    none

--- src/content/ModItem.java
+++ src/content/ModItem.java
@@ -1,14 +1,22 @@
 package content;
 
-import mindustry.type.Item;
+import arc.graphics.Color;
+import mindustry.mod.*;
+import mindustry.type.*;
 
-public class ModItem extends Item{
-    public ModItem(String name){
-        super(name);
-    }
-    public static ModItem apple;
-    @Override
-    public void load(){
-        apple = new ModItem("apple");
+public class ModItem extends Mod {
+    public static Item apple;
+
+    public void loadcontent(){
+        apple = new Item("apple"){
+            {
+                localizedName = "示例资源";
+                description = "一种用于示例模组的特殊资源";
+                color = Color.valueOf("84f542"); // 设置颜色
+                hardness = 1;
+                cost = 1.5f;
+                alwaysUnlocked = true; // 不需要研究解锁
+            }
+        };
     }
 }
